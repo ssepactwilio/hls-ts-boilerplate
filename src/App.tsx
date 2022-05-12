3353927f83ea818ea159838458e77fced0c59013
ssepactwilio <ssepac@twilio.com>
2022-05-11 20:41:38 -0600

    added serverless

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  useEffect(()=>{
-    var a = 4
-  })
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit
+          {' '}
+          <code>src/App.tsx</code>
+          {' '}
+          and save to reload.
         </p>
         <a
           className="App-link"
